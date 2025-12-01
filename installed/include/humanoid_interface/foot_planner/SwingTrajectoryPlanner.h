753219191680e7c92e3d52c9aac52403fcd30be2
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2025-12-01 18:41:14 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/728b99cf87cbee18ca4c3dea12dee0e78ad7bd0a

--- installed/include/humanoid_interface/foot_planner/SwingTrajectoryPlanner.h
+++ installed/include/humanoid_interface/foot_planner/SwingTrajectoryPlanner.h
@@ -67,6 +67,7 @@ class SwingTrajectoryPlanner {
     scalar_t heelToeMaxHeightVelocity = 0.5;
     scalar_t next_position_z = 0.0;
 
+    scalar_t swing_shoulder_roll_center = 0.0;
     scalar_t swing_shoulder_center = 0.2;
     scalar_t swing_shoulder_scale = 0.2;
     scalar_t swing_elbow_center = 0.0;
