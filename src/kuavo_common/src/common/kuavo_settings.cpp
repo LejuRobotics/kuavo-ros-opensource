38befd9579b43752625e1fcb979a09e95a5d24ab
kuavo_ci2 <kuavo_ci2@lejurobot.com>
2026-02-11 11:51:10 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/3e6ed0095cdadf4460cefe12099d4f8c8901cd00

--- src/kuavo_common/src/common/kuavo_settings.cpp
+++ src/kuavo_common/src/common/kuavo_settings.cpp
@@ -229,6 +229,7 @@ namespace HighlyDynamic
                 {"PA72_36_R", {BIT_17_36, PA72_36_MC, PA72_36_C2T, EC_MASTER}},
                 {"PA76_25", {BIT_17_25, PA76_25_MC, PA76_25_C2T, EC_MASTER}},
                 {"PA76_18", {BIT_17_18, PA76_18_MC, PA76_18_C2T, EC_MASTER}},
+                {"PA81_18_25", {BIT_17_25, PA81_18_25_MC, PA81_18_25_C2T, EC_MASTER}},
                 {"PA105_18", {BIT_17_18, PA105_18_MC, PA105_18_C2T, EC_MASTER}},
                 {"PA81_25", {BIT_17_251, PA81_25_MC, PA81_25_C2T, EC_MASTER}},
                 {"PA4315_36", {BIT_17_36, PA4315_36_MC, PA4315_36_C2T, EC_MASTER}}};
