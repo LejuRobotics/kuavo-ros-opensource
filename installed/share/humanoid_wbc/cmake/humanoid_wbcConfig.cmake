eedc0c01a6a847fb62d5509a242a91ef0368b779
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2026-03-16 16:31:28 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/015d0693f5673e78766fb390f70ffed2ecb4aa7d

--- installed/share/humanoid_wbc/cmake/humanoid_wbcConfig.cmake
+++ installed/share/humanoid_wbc/cmake/humanoid_wbcConfig.cmake
@@ -67,14 +67,14 @@ set(humanoid_wbc_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("FALSE" STREQUAL "TRUE")
-  set(humanoid_wbc_SOURCE_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/src/humanoid-control/humanoid_wbc)
-  set(humanoid_wbc_DEVEL_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/devel)
+  set(humanoid_wbc_SOURCE_PREFIX /media/data_old/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/src/humanoid-control/humanoid_wbc)
+  set(humanoid_wbc_DEVEL_PREFIX /media/data_old/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/devel)
   set(humanoid_wbc_INSTALL_PREFIX "")
   set(humanoid_wbc_PREFIX ${humanoid_wbc_DEVEL_PREFIX})
 else()
   set(humanoid_wbc_SOURCE_PREFIX "")
   set(humanoid_wbc_DEVEL_PREFIX "")
-  set(humanoid_wbc_INSTALL_PREFIX /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed)
+  set(humanoid_wbc_INSTALL_PREFIX /media/data_old/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed)
   set(humanoid_wbc_PREFIX ${humanoid_wbc_INSTALL_PREFIX})
 endif()
 
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed/lib;/media/data/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed/lib;/opt/ros/noetic/lib)
+    foreach(path /media/data_old/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed/lib;/media/data_old/gitlab-runner/builds/8rHoW4Dt/0/highlydynamic/kuavo-ros-control/installed/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
