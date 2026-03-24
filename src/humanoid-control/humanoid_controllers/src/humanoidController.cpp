38940f1c8e25cae50f9002bcdf78bf2d8940097a
kuavo_CI <kuavo_CI@50_eb_f6_bd_54_c1>
2026-03-24 17:03:30 +0800

    sync https://www.lejuhub.com/highlydynamic/kuavo-ros-control/commit/9a5dd1fa27ba7565549108d50c5e293e9c0fce21

--- src/humanoid-control/humanoid_controllers/src/humanoidController.cpp
+++ src/humanoid-control/humanoid_controllers/src/humanoidController.cpp
@@ -1843,11 +1843,11 @@ void humanoidController::sensorsDataCallback(const kuavo_msgs::sensorsData::Cons
     // currentObservation_.state(8) = 0.78626;
     // currentObservation_.state.segment(6 + 6, jointNum_) = defalutJointPos_;
     initial_status_ = HumanoidInterface_->getInitialState();
-    // 判断当为ROBAN时才进行赋值（使用显式 segment 避免 .tail 引起的索引歧义）
     if (is_roban_) {
       int arm_start = 12 + jointNumReal_ + waistNum_;
       if (static_cast<int>(initial_status_.size()) >= arm_start + static_cast<int>(armNumReal_)) {
         initial_status_.segment(arm_start, armNumReal_) = defalutArmPosMPC_;
+        arm_joint_pos_filter_.reset(defalutArmPosMPC_);
       } else {
         ROS_WARN_STREAM("[starting] initial_status_.size()=" << initial_status_.size()
                         << " too small to set arm segment at " << arm_start
